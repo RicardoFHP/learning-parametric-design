c929e2a3d3adc11334f5fb204de753fcbc28dc94
Ricardo Engels <ricardo.engels@fh-potsdam.de>
2022-05-04 12:07:14 +0200

    task-07
    Curve

---
+++ code/task-07/sketch.js
@@ -0,0 +1,43 @@
+function preload() {
+  // preload assets
+}
+
+const sketchWidth = 400;
+const sketchHeight = 450;
+
+function setup() {
+  createCanvas(sketchWidth, sketchHeight);
+}
+
+
+
+function draw() {
+  stroke(80);
+  strokeWeight:(3);
+  //fill(0);
+  // noFill();
+  
+  beginShape();
+
+  frameRate (10);
+  vertex(0, 0);
+  vertex(40, 0);
+  
+  for (let y = 1; y < 6; y += 1.5) {
+    const x = noise(5);
+    const cX = noise(5);
+    let direct = 91;
+    if (y%2===0) {
+      direct = 1;
+    }
+
+    curveVertex(100 + cX * direct, 400 / 5 * y - (400 / 5 * 0.5), 200 + x * direct * -1, 400 / 5 * y)
+  }
+
+  vertex(0, 400);
+  vertex(0, 400);
+  11
+  endShape(CLOSE);
+
+  draw(30);
+}
